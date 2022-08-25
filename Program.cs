84819caea1e57937cffb62a3aaca1b42ff5ce01e
jyoti <jyotidhage10@gmail.com>
2022-08-25 18:58:00 +0530

    [Aaradhya]Add:Enter user Valid First Name

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ValidationUsingRegex
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            RegexValidation regexValidation = new RegexValidation();
+            bool RegexResult1 = regexValidation.validateFirstName();
+            Console.WriteLine("firstname"+ " "  +RegexResult1);
+        }
+    }
+}
---
+++ RegexValidation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ValidationUsingRegex
+{
+    public  class RegexValidation
+    {
+        public static string FIRST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3}$";
+       
+       
+     //   public static string LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z}{3}$";
+
+       
+       // public static string EMAIL_REGEX = "^[a-z0-9+_.-]+@[A-Z0-9.-]+$";
+
+
+      //  public static string MOBILENUMBER_REGEX = "^[+91]{1}[6-9]{1}[0-9]{9}$";
+
+
+      //  public static string PASSWORD_REGEX = "^[[A_Z]{1}[A_Za-z][0-9]{1}[./#/@/%]{1]]{8}$";
+
+
+          public  bool validateFirstName()
+        {
+            return Regex.IsMatch("Teju", FIRST_NAME_REGEX);
+        }
+
+
+
+    }
+}
