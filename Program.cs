595032b6c7d5b93d4a1a164c1a97bcdc016a905e
jyoti <jyotidhage10@gmail.com>
2022-08-25 22:07:09 +0530

    [Aaradhya]Add: At least 1 character in Uppercase

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@
             // Console.WriteLine("lastname"+ "  "    +RegexResult2);
 
             // bool RegexResult3 = regexValidation.validateEmail();
-            // Console.WriteLine("Email" + "  " + RegexResult3);
+            //Console.WriteLine("Email" + "  " + RegexResult3);
 
             //  bool RegexResult4 = regexValidation.validateMobileNumber();
             //Console.WriteLine("MobileNumber" + "  " + RegexResult4);
